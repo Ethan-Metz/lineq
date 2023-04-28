55c933f64e6e6dc1dc73d81447931b1cc5ef141b
Ethan-Metz <90403707+Ethan-Metz@users.noreply.github.com>
2023-04-27 21:15:00 -0400

    Update lib.rs added third example

--- src/lib.rs
+++ src/lib.rs
@@ -19,16 +19,35 @@
 //! assert_eq!(a.cross(b),Vec3::IN);
 //! '''
 //!
-//! Arrays of vectors can be made to parallelize arithmetic:
+//! Arrays of vectors can be used to parallelize arithmetic:
 //! '''rust
 //! # extern crate lineq
 //! use lineq::vec3::Vec3;
+//! use lineq::vec3arr::Vec3arr;
 //! //using a and b from last example:
 //! # let a : Vec3 = Vec3::UP;
 //! # let b : Vec3 = Vec3::LEFT;
 //! let ab : Vec3arr = Vec3arr([a,b]);
 //! let cd : Vec3arr = Vec3arr([Vec3::DOWN,Vec3::RIGHT]);
 //! assert_eq!(ab + cd, Vec3arr([Vec3::ZERO,Vec3::ZERO]));
+//! '''
+//!
+//! Matricies are indexed like x1, y2, z3 ... where x, y, z
+//! are the rows and 1, 2, 3 are the columns:
+//! '''rust
+//! # extern crate lineq
+//! use lineq::vec3::Vec3;
+//! use lineq::mat33::Mat33;
+//! //using a and b from first example:
+//! # let a : Vec3 = Vec3::UP;
+//! # let b : Vec3 = Vec3::LEFT;
+//! let c : Vec3 = Vec3{ x: 1.0, y: -1.0, z: 2.0 };
+//! let m1 : Mat33 = Mat33::augment(a,b,c);
+//! let m2 : Mat33 = Mat33{
+//! x1: 0.0, x2: -1.0, x3: 1.0,
+//! y1: 1.0, y2: 0.0, y3: -1.0,
+//! z1: 0.0, z2: 0.0, z3: 2.0 }
+//! assert_eq!(m1,m2);
 //! '''
 
 #![feature(new_uninit)]
