de5475e7f82fd838653632268ebc17ee942acb2d
Ethan-Metz <90403707+Ethan-Metz@users.noreply.github.com>
2023-04-27 21:58:28 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,16 @@
 //!   [Mat33](crate::mat33::Mat33).
 //!
 //! Vectors can be easily initialized and used:
-//! '''rust
+//! ```rust
 //! # extern crate lineq
 //! use lineq::vec3::Vec3;
 //! let a : Vec3 = Vec3::UP;
 //! let b : Vec3 = Vec3 { x: -1.0, y: 0.0, z: 0.0 };
 //! assert_eq!(a.cross(b),Vec3::IN);
-//! '''
+//! ```
 //!
 //! Arrays of vectors can be used to parallelize arithmetic:
-//! '''rust
+//! ```rust
 //! # extern crate lineq
 //! use lineq::vec3::Vec3;
 //! use lineq::vec3arr::Vec3arr;
@@ -30,11 +30,11 @@
 //! let ab : Vec3arr = Vec3arr([a,b]);
 //! let cd : Vec3arr = Vec3arr([Vec3::DOWN,Vec3::RIGHT]);
 //! assert_eq!(ab + cd, Vec3arr([Vec3::ZERO,Vec3::ZERO]));
-//! '''
+//! ```
 //!
 //! Matricies are indexed like x1, y2, z3 ... where x, y, z
 //! are the rows and 1, 2, 3 are the columns:
-//! '''rust
+//! ```rust
 //! # extern crate lineq
 //! use lineq::vec3::Vec3;
 //! use lineq::mat33::Mat33;
@@ -48,7 +48,7 @@
 //! y1: 1.0, y2: 0.0, y3: -1.0,
 //! z1: 0.0, z2: 0.0, z3: 2.0 }
 //! assert_eq!(m1,m2);
-//! '''
+//! ```
 
 #![feature(new_uninit)]
 #![feature(slice_ptr_get)]
