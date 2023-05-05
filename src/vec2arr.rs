ccffa410de05b84e72490b3d3447edff326023ef
Ethan-Metz <90403707+Ethan-Metz@users.noreply.github.com>
2023-05-04 21:23:17 -0400

    Update vec2arr.rs

--- src/vec2arr.rs
+++ src/vec2arr.rs
@@ -68,11 +68,7 @@ impl DerefMut for Vec2raw {
 use std::ops::Add;
 use std::ops::Index;
 	
-impl<T, const N: usize> Add<T> for Vec2arr<N>
-where
-    T: !Index,
-    <T as Index>::I: ?Sized,
-{
+impl<T: Add<Vec2>, const N: usize> Add<T> for Vec2arr<N> {
     type Output = Vec2arr<N>;
     #[inline]
     fn add(self, rhs: T) -> Vec2arr<N> {
