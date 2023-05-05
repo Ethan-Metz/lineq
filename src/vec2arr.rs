0721b04339d5cac3025abd9437753a734ea319ac
Ethan-Metz <90403707+Ethan-Metz@users.noreply.github.com>
2023-05-04 23:21:07 -0400

    Update vec2arr.rs

--- src/vec2arr.rs
+++ src/vec2arr.rs
@@ -136,19 +136,19 @@ where
 
 use std::slice::SliceIndex;
 
-impl<T::<N>, I, const N: usize> Add<T::<N>> for f32
+impl<T, const N: usize> Add<T> for f32
 where 
-    T: Index<I, Output = Vec2>,
+    T: Deref<Target = [Vec2,N]>,
     f32: Add<Vec2, Output = Vec2>,
 {
     type Output = T::<N>;
     #[inline]
-    fn add(self, rhs: T::<N>) -> T::<N> {
-        let mut tmp: T::<N> = unsafe { MaybeUninit::uninit().assume_init() };
+    fn add(self, rhs: T) -> T::<N> {
+        let mut tmp: T = unsafe { MaybeUninit::uninit().assume_init() };
         for i in 0..N {
             tmp[i] = self + rhs[i];
         }
-        unsafe { std::mem::transmute::<_, T::<N>>(tmp) }
+        unsafe { std::mem::transmute::<_, T>(tmp) }
     }
 }
 
