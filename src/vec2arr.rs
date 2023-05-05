491045352ce7f041fcffdacedec0402a1dc785e6
Ethan-Metz <90403707+Ethan-Metz@users.noreply.github.com>
2023-05-04 22:00:56 -0400

    Update vec2arr.rs

--- src/vec2arr.rs
+++ src/vec2arr.rs
@@ -70,7 +70,7 @@ use std::ops::Index;
 	
 impl<T, const N: usize> Add<T> for Vec2arr<N>
 where 
-    Vec2: Add<Rhs = T, Output = Vec2>,
+    Vec2: Add<T, Output = Vec2>,
 {
     type Output = Vec2arr<N>;
     #[inline]
