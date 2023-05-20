d3095f8797f6f95333e8ddf96f85b0da6b15843e
Ethan-Metz <90403707+Ethan-Metz@users.noreply.github.com>
2023-05-19 22:51:10 -0400

    Update vec2arr.rs

--- src/vec2arr.rs
+++ src/vec2arr.rs
@@ -216,7 +216,7 @@ trait BoxHelper<ID, Rhs> { //used when lhs is a box type
 }
 
 // blanket impl 1
-impl<const N: usize, T: VArr<N>, Rhs> BoxHelper<i8, Rhs> for T { //Rhs is array type
+impl<T: VArr, Rhs> BoxHelper<i8, Rhs> for T { //Rhs is array type
     type AddType = Vec2arr<N>;
     fn add_imp(self, rhs: Rhs) -> Vec2arr<N> {
         if self.len() != rhs.len() { panic!("slice and array inequal length"); }
